e1676fdd1788fd14f52970332171d711bd9ff9e2
caiopires92 <caiogodoi_92@hotmail.com>
2026-01-30 12:55:20 -0300

    feat: adicionar botao flutuante do WhatsApp

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -28,6 +28,7 @@ export const metadata: Metadata = {
 
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import WhatsAppFloatingButton from "@/components/WhatsAppFloatingButton";
 
 export default function RootLayout({
   children,
@@ -42,6 +43,7 @@ export default function RootLayout({
         <Header />
         {children}
         <Footer />
+        <WhatsAppFloatingButton />
       </body>
     </html>
   );
